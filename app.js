157b873c25b4445b4c5bd9814d7c995eccba6161
daublej <jiangjiedeyx@163.com>
2017-11-25 22:52:16 +0800

    刮刮乐第一版

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var express = require('express')    //加载express模块
-var app = express()
-	var port = process.env.PORT || 3000  //监听的端口
-app.use(express.static(__dirname + '/static'))
+var express = require('express')  ;  //加载express模块
+var app = express();
+	var port = process.env.PORT || 3000 ; //监听的端口
+app.use(express.static(__dirname + '/static'));
 app.get('/loader', function (req, res) {
    console.log("/loader");
    res.sendfile('./static/loader.js');
-})
+});
 app.use(function (req , res){
    res.sendfile('./static/index.html')
-})
+});
 
 app.listen(port,function(){
 		console.log('TechNode is on port' + port + '!' ) 
-})
+});
